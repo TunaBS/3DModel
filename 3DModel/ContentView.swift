a3010ad2d5904f8a658d771fb86ae2515831d698
fairuz_ahmed_tuna <fairuz.ahmed@brainstation-23.com>
2024-08-19 18:02:09 +0600

    Initial Commit

---
+++ 3DModel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  3DModel
+//
+//  Created by BS00880 on 19/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 3DModel/_DModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DModelApp.swift
+//  3DModel
+//
+//  Created by BS00880 on 19/8/24.
+//
+
+import SwiftUI
+
+@main
+struct _DModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
